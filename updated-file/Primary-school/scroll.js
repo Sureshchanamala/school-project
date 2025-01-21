c1225cf4bdcbd23af2126eea2cd4dc0d6ec8b8c1
Sureshchanamala <sureshchanamala9@gmail.com>
2025-01-21 08:54:43 +0000

    updated-file

---
+++ updated-file/Primary-school/scroll.js
@@ -0,0 +1,12 @@
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "inline-block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
